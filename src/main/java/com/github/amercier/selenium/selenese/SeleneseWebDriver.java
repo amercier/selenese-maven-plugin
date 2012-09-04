9db522241df47c926049b905b74e94e5ccd965d6
amercier <alexandre.mercier12@gmail.com>
2012-09-04 18:34:13 +0200

    Added action check

--- src/main/java/com/github/amercier/selenium/selenese/SeleneseWebDriver.java
+++ src/main/java/com/github/amercier/selenium/selenese/SeleneseWebDriver.java
@@ -83,14 +83,15 @@ public class SeleneseWebDriver extends RemoteWebDriver {
 				break; case assertElementNotPresent : Assert.assertNull(this.findElement(ElementLocator.parse(command.getArgument(0))), "Can find element \"" + command.getArgument(0) + "\"");
 				break; case assertLocation          : Assert.assertPatternMatches(parsePattern(command.getArgument(0)), getCurrentUrl());
 				break; case click                   : findElement(ElementLocator.parse(command.getArgument(0))).click();
+				break; case check                   : { WebElement e = findElement(ElementLocator.parse(command.getArgument(0))); if(e.getAttribute("checked") != null) e.click(); }
 				break; case getEval                 : executeScript(command.getArgument(0), new Object[0]);
 				break; case echo                    : System.out.println(executeScript("return (" + command.getArgument(0) + ")", new Object[0]));
 				break; case open                    : get(getAbsoluteURL(command.getArgument(0)));
 				break; case pause                   : pause(Long.parseLong(command.getArgument(0)));
-				break; case type                    : WebElement e = findElement(ElementLocator.parse(command.getArgument(0))); e.clear(); e.sendKeys(command.getArgument(1));
+				break; case type                    : { WebElement e = findElement(ElementLocator.parse(command.getArgument(0))); e.clear(); e.sendKeys(command.getArgument(1)); }
 				break; case select                  : findElement(ElementLocator.parse(command.getArgument(0))).findElement(OptionLocator.parse(command.getArgument(1))).click();
 				break; case storeEval               : storage.put(command.getArgument(1), "" + executeScript("return (" + command.getArgument(0) + ")", new Object[0]));
-				break; case waitForElementNotPresent: final By by = ElementLocator.parse(command.getArgument(0)); (new WebDriverWait(this, 10)).until(new ExpectedCondition<WebElement>(){ public WebElement apply(WebDriver d) { return d.findElement(by);	}});
+				break; case waitForElementNotPresent: { final By by = ElementLocator.parse(command.getArgument(0)); (new WebDriverWait(this, 10)).until(new ExpectedCondition<WebElement>(){ public WebElement apply(WebDriver d) { return d.findElement(by); }}); }
 			}
 		}
 		catch(InvalidSeleneseCommandArgumentException e) {
