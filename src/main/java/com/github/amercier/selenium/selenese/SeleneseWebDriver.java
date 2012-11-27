428f3ab11f8d128b62f5e6e6a3a7028652317fe0
amercier <alexandre.mercier12@gmail.com>
2012-11-27 15:47:13 +0100

    Fixed dradAndDropToObjet command not performing

--- src/main/java/com/github/amercier/selenium/selenese/SeleneseWebDriver.java
+++ src/main/java/com/github/amercier/selenium/selenese/SeleneseWebDriver.java
@@ -94,7 +94,7 @@ public class SeleneseWebDriver extends RemoteWebDriver {
 				break; case assertText              : Assert.assertPatternMatches(parsePattern(command.getArgument(1)), findElement(ElementLocator.parse(command.getArgument(0))).getText());
 				break; case click                   : findElement(ElementLocator.parse(command.getArgument(0))).click();
 				break; case check                   : { WebElement e = findElement(ElementLocator.parse(command.getArgument(0))); if(e.getAttribute("checked") == null) e.click(); }
-				break; case dragAndDropToObject     : (new Actions(this)).dragAndDrop( findElement(ElementLocator.parse(command.getArgument(0))), findElement(ElementLocator.parse(command.getArgument(1))) );
+				break; case dragAndDropToObject     : (new Actions(this)).dragAndDrop( findElement(ElementLocator.parse(command.getArgument(0))), findElement(ElementLocator.parse(command.getArgument(1))) ).perform();
 				break; case getEval                 : executeScript(command.getArgument(0), new Object[0]);
 				break; case echo                    : System.out.println(executeScript("return ('" + command.getArgument(0) + "')", new Object[0]));
 				break; case open                    : get(getAbsoluteURL(command.getArgument(0)));
