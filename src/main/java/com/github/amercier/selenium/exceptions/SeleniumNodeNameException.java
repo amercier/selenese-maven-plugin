86d5fefd8e7f6954c38a49d3690e88f0633eda12
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-04-11 09:31:29 +0200

    Added NodeNameException

---
+++ src/main/java/com/github/amercier/selenium/exceptions/SeleniumNodeNameException.java
@@ -0,0 +1,9 @@
+package com.github.amercier.selenium.exceptions;
+
+@SuppressWarnings("serial")
+public class SeleniumNodeNameException extends Exception {
+	
+	public SeleniumNodeNameException(String message) {
+		super(message);
+	}
+}
